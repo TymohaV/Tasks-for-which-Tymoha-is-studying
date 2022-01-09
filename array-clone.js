5e77c94a8ab1bbcf30f821ef01791f223ea11025
Vladyslav <vlados230761@gmail.com>
2022-01-09 17:43:27 +0200

    new long metod clone array

--- array-clone.js
+++ array-clone.js
@@ -1,12 +1,23 @@
+// let vegetables = ['Капуста', 'Морковка', 'Ридиска'];
+
+// function arrayClone(arr) {
+//         return arr.slice(0);
+
+
+// };
+
 let vegetables = ['Капуста', 'Морковка', 'Ридиска'];
 
 function arrayClone(arr) {
-        return arr.slice(0);
-        
-    
+        let cloneVeg = [];
+        for (i = 0; i < arr.length; i++) {
+                let currentVeg = arr[i];
+                cloneVeg.push(currentVeg);
+                
+        }
+        return cloneVeg;
 };
 
-
-
-
+let result = arrayClone(vegetables);
+console.log(result);
 
