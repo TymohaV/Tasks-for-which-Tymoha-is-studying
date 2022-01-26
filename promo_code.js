d8d41f4b5028c1c0af53acdf4fae13f1cd0c9711
Vladyslav <vlados230761@gmail.com>
2022-01-27 00:31:28 +0200

    task from react maraphone

--- promo_code.js
+++ promo_code.js
@@ -1,19 +1,19 @@
 
 
-let promoCode = 31364452;
+// let promoCode = 31364452;
 
-function promoCodeFunc(promoCode) {
-    let resultArray = [];
-    while (promoCode > 0) {
-        let num = promoCode % 10;
-        resultArray.push(num);
-        promoCode = (promoCode - num) / 10;
-    } return resultArray; 
+// function promoCodeFunc(promoCode) {
+//     let resultArray = [];
+//     while (promoCode > 0) {
+//         let num = promoCode % 10;
+//         resultArray.push(num);
+//         promoCode = (promoCode - num) / 10;
+//     } return resultArray;
 
 
-}
-;
-console.log(promoCodeFunc(promoCode));
+// }
+// ;
+// console.log(promoCodeFunc(promoCode));
 
 
 
@@ -28,4 +28,18 @@ console.log(promoCodeFunc(promoCode));
 
 // console.log(resultArray.reverse().join(','));
 
+let str = 'welcone to react maraphone Vladysvav';
+
+function changeWordsOrder(str) {
+    const strWorsd = str.split(' ');
+    strWorsd.sort(function (a, b) {
+       
+        return b.length - a.length;
+        
+    })
+    
+    console.log(strWorsd.join(' '));
+
+};
+changeWordsOrder(str);
 
