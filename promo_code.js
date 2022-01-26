5acceff0824d88eed6b5a651519b436a24b557d0
Vladyslav <vlados230761@gmail.com>
2022-01-27 00:40:18 +0200

    task from react marathone

--- promo_code.js
+++ promo_code.js
@@ -1,32 +1,33 @@
+// tasks promocode-----------------------------
+
+let promoCode = 31364452;
+
+function promoCodeFunc(promoCode) {
+    let resultArray = [];
+    while (promoCode > 0) {
+        let num = promoCode % 10;
+        resultArray.push(num);
+        promoCode = (promoCode - num) / 10;
+    } return resultArray;
 
 
-// let promoCode = 31364452;
-
-// function promoCodeFunc(promoCode) {
-//     let resultArray = [];
-//     while (promoCode > 0) {
-//         let num = promoCode % 10;
-//         resultArray.push(num);
-//         promoCode = (promoCode - num) / 10;
-//     } return resultArray;
-
-
-// }
-// ;
-// console.log(promoCodeFunc(promoCode));
+}
+;
+console.log(promoCodeFunc(promoCode));
 
 
 
+let promoCode = 12779057;
+let resultArray = [];
 
-// let promoCode = 12779057;
-// let resultArray = [];
+while (promoCode) {
+    resultArray.push(promoCode % 10);
+    promoCode = Math.floor(promoCode / 10);
+}
 
-// while (promoCode) {
-//     resultArray.push(promoCode % 10);
-//     promoCode = Math.floor(promoCode / 10);
-// }
+console.log(resultArray.reverse().join(','));
 
-// console.log(resultArray.reverse().join(','));
+// task from react marathon------------------------------------------
 
 let str = 'welcone to react maraphone Vladysvav';
 
