8d36b0a1a1e1fc59e0f74fd34fa7126998cc79f0
Vladyslav <vlados230761@gmail.com>
2022-02-12 13:50:53 +0200

    new tasks

--- promo_code.js
+++ promo_code.js
@@ -1,31 +1,31 @@
 // tasks promocode-----------------------------
 
-let promoCode = 31364452;
+// let promoCode = 31364452;
 
-function promoCodeFunc(promoCode) {
-    let resultArray = [];
-    while (promoCode > 0) {
-        let num = promoCode % 10;
-        resultArray.push(num);
-        promoCode = (promoCode - num) / 10;
-    } return resultArray;
-
-
-}
-;
-console.log(promoCodeFunc(promoCode));
+// function promoCodeFunc(promoCode) {
+//     let resultArray = [];
+//     while (promoCode > 0) {
+//         let num = promoCode % 10;
+//         resultArray.push(num);
+//         promoCode = (promoCode - num) / 10;
+//     } return resultArray;
 
 
+// }
+// ;
+// console.log(promoCodeFunc(promoCode));
 
-let promoCode = 12779057;
-let resultArray = [];
 
-while (promoCode) {
-    resultArray.push(promoCode % 10);
-    promoCode = Math.floor(promoCode / 10);
-}
 
-console.log(resultArray.reverse().join(','));
+// let promoCode = 12779057;
+// let resultArray = [];
+
+// while (promoCode) {
+//     resultArray.push(promoCode % 10);
+//     promoCode = Math.floor(promoCode / 10);
+// }
+
+// console.log(resultArray.reverse().join(','));
 
 // task from react marathon------------------------------------------
 
